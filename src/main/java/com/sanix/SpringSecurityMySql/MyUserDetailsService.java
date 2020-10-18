3b1a6ca0672545f1dfb7c76a29e0c1e7a94d9559
sanix <sanicksikani@gmail.com>
2020-10-18 08:03:06 +0300

    Completed

--- src/main/java/com/sanix/SpringSecurityMySql/MyUserDetailsService.java
+++ src/main/java/com/sanix/SpringSecurityMySql/MyUserDetailsService.java
@@ -1,15 +1,30 @@
 package com.sanix.SpringSecurityMySql;
 
+import com.sanix.SpringSecurityMySql.models.MyUserDetails;
+import com.sanix.SpringSecurityMySql.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException{
-        return new MyUserDetails(s);
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException{
+
+        Optional <User> user=userRepository.findByUserName(userName);
+
+        user.orElseThrow(()->new UsernameNotFoundException("Not Found: "+userName));
+
+        return user.map(MyUserDetails::new).get();
+
+
     }
 }
--- src/main/java/com/sanix/SpringSecurityMySql/models/MyUserDetails.java
+++ src/main/java/com/sanix/SpringSecurityMySql/models/MyUserDetails.java
@@ -1,4 +1,4 @@
-package com.sanix.SpringSecurityMySql;
+package com.sanix.SpringSecurityMySql.models;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -6,28 +6,38 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyUserDetails implements UserDetails {
 
-    private String username;
+    private String userName;
+    private String password;
+    private boolean active;
+    private List<GrantedAuthority> authorities;
 
-    public MyUserDetails(String username) {
-        this.username=username;
+    public MyUserDetails(User user) {
+        this.userName=user.getUserName();
+        this.password=user.getPassword();
+        this.active=user.isActive();
+        this.authorities=Arrays.stream(user.getRoles().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+       return authorities;
     }
 
     @Override
     public String getPassword() {
-        return "pass";
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return username;
+        return userName;
     }
 
     @Override
@@ -47,6 +57,6 @@ public class MyUserDetails implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return true;
+        return active;
     }
 }
